08aeaad7e3c4ac1f9d36b74f5906ce97760b0d5b
banan213 <artur3aika@gmail.com>
2025-10-24 11:46:27 +0300

    Lab 1

---
+++ laba1/go.mod
@@ -0,0 +1,3 @@
+module laba1
+
+go 1.25.1
---
+++ laba1/laba1.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+)
+
+func main() {
+	mali := "qwertyuiopasdfghjklzxcvbnm"
+	veliki := "QWERTYUIOPASDFGHJKLZXCVBNM"
+	spec := "!@#$%^&*()-_=+[]{}<>?/|"
+	num := "1234567890"
+	pas := ""
+
+	var IFmali string
+	var IFveliki string
+	var IFspec string
+	var IFnum string
+	var pul string
+	var vs int
+
+	fmt.Print("Введіть довжину пароля: ")
+	var lenStr string
+	fmt.Scanln(&lenStr)
+	lan, err := strconv.Atoi(lenStr)
+	if err != nil {
+		fmt.Println("Введено не число")
+		return
+	}
+
+	if lan <= 4 || lan >= 128 {
+		fmt.Println("некоректна довжина пароля")
+	}
+
+	for {
+
+		fmt.Print("Використовувати малі літери? (y/n):")
+		fmt.Scanln(&IFmali)
+		if IFmali == "y" {
+			pul = pul + mali
+			vs++
+
+		}
+
+		fmt.Print("Використовувати великі літери? (y/n):")
+		fmt.Scanln(&IFveliki)
+		if IFveliki == "y" {
+			pul = pul + veliki
+			vs++
+		}
+
+		fmt.Print("Використовувати спец. символи? (y/n):")
+		fmt.Scanln(&IFspec)
+		if IFspec == "y" {
+			pul = pul + spec
+			vs++
+		}
+
+		fmt.Print("Використовувати цифри? (y/n):")
+		fmt.Scanln(&IFnum)
+		if IFnum == "y" {
+			pul = pul + num
+			vs++
+		}
+
+		if vs == 0 {
+			fmt.Println("ви не обрали жодну категорію")
+		} else {
+			break
+		}
+	}
+
+	for i := 0; i < lan; i++ {
+		idx := rand.Intn(len(pul))
+		s := string(pul[idx])
+		pas = pas + s
+	}
+	fmt.Println("згенорований пароль:", pas)
+}
+
+// func main() {
+// 	s := "Hello, World"
+
+// 	idx := rand.Intn(len(s))
+
+// 	str := string(s[idx])
+// 	fmt.Println("random character:", str)
+// }
+
+// func main() {
+// 	var name string
+// 	var age int
+
+// 	fmt.Print("Введите имя: ")
+// 	fmt.Scanln(&name)
+
+// 	fmt.Print("Введите возраст: ")
+// 	fmt.Scanln(&age)
+
+// 	fmt.Println("Привет,", name, "Тебе", age, "лет")
+// }
